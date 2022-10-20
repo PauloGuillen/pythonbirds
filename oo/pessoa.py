b67373cbca8ebb30b5702595c9832f51e8c90f77
PauloGuillen <psm.guillen@gmail.com>
2022-10-19 21:37:15 -0300

    criado o atributo número de olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    numero_olhos = 2
+
     def __init__(self, *filhos, nome = None, idade=59) -> None:
         self.nome = nome
         self.idade =idade
@@ -8,11 +10,22 @@ class Pessoa:
         print(f'Olá {id(self)}')
 
 if __name__ == '__main__':
-    paulo = Pessoa(nome='Paulo')
-    isabel = Pessoa(nome='Isabel')
-    francisco = Pessoa(isabel,paulo, nome='Francisco')
+    paulo = Pessoa('Isadora', nome='Paulo')
+    isabel = Pessoa(nome='Isabel', idade=63)
+    francisco = Pessoa(isabel,paulo, nome='Francisco', idade=89)
     print(paulo.nome)
     print(francisco.nome)
     for filho in francisco.filhos:
         print(filho.nome)
-
+    paulo.sobrenome = 'Guillén'
+    print(francisco.__dict__)
+    print(isabel.__dict__)
+    print(paulo.__dict__)
+    Pessoa.numero_olhos = 3
+    print(Pessoa.numero_olhos)
+    print(paulo.numero_olhos)
+    print(id(Pessoa.numero_olhos), id(paulo.numero_olhos), id(francisco.numero_olhos))
+    paulo.numero_olhos = 1
+    print(paulo.numero_olhos)
+    print(id(Pessoa.numero_olhos), id(paulo.numero_olhos), id(francisco.numero_olhos))
+    print(paulo.__dict__)
