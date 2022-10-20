a47f211c4a3dad7639514293022f382949a83141
PauloGuillen <psm.guillen@gmail.com>
2022-10-19 21:34:09 -0300

    criado o atributo olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,18 @@
 class Pessoa:
-    pass
+    def __init__(self, *filhos, nome = None, idade=59) -> None:
+        self.nome = nome
+        self.idade =idade
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        print(f'Olá {id(self)}')
+
+if __name__ == '__main__':
+    paulo = Pessoa(nome='Paulo')
+    isabel = Pessoa(nome='Isabel')
+    francisco = Pessoa(isabel,paulo, nome='Francisco')
+    print(paulo.nome)
+    print(francisco.nome)
+    for filho in francisco.filhos:
+        print(filho.nome)
+
